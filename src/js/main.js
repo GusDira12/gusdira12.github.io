452e89c16008d022bd1272463165c46a7959b2d7
GusDira12 <diravi1814@gmail.com>
2025-04-02 19:00:26 +0900

    replaced with a rework done a month ago...

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ const counter1 = "you've clicked my logo 1 time!"
 function buttonMasher() {
     clicks += 1;
 
-    const imgTag = document.getElementById("logo").querySelector("img"); // image tag
+    const textTag = document.getElementById("innerWrap").getElementsByClassName("text-tag").item(0); // text tag
     const counter = "you've clicked my logo " + clicks + " times!";
 
     // set title to update amount of clicks
-    if (clicks == 1) imgTag.setAttribute("title", counter1);
-    else imgTag.setAttribute("title", counter);
+    if (clicks == 1) textTag.innerHTML = counter1;
+    else textTag.innerHTML = counter;
 }
