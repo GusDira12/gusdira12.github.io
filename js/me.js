c1426caf0c31b65919892ac17b69b994531d98cf
GusDira12 <diravi1814@gmail.com>
2025-04-03 17:30:15 +0900

    saut!

--- js/me.js
+++ js/me.js
@@ -1,23 +1,19 @@
 // -= global things.. =-
 
-var innerWrapTag = "";
-var textMainTag = "";
+var innerWrapTag, profileGusTag, profileSautTag = "";
 
 window.onload = function() {
     innerWrapTag = document.getElementById("innerWrap"); 
-    textMainTag = innerWrapTag.getElementsByClassName("text-tag"); // only first child  
+    profileGusTag = document.getElementById("profile-gus");
+    profileSautTag = document.getElementById("profile-saut");
 }
 
 // -= changes a thing or 2 from gus to saut =-
 
 function changeGusToSaut() {
-    textMainTag.item(0).hidden = true;
-    textMainTag.item(1).hidden = false;
+    profileGusTag.setAttribute("style", "visibility: hidden; max-height: 0;");
+    profileSautTag.setAttribute("style", "");
 
     var titleTag = document.getElementsByTagName("title");
     titleTag.item(0).innerHTML = "who is this saut person?";
-
-    var photoTag = innerWrapTag.getElementsByTagName("img").item(0);
-    photoTag.src = "./img/scooters/saut.png";
-    photoTag.title = "I'm no longer being used";
 }
